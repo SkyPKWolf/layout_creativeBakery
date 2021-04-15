11053fa263aecad8f7f8c4b64bd84da2cd5e4e17
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-04-15 16:42:22 +0300

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const toggler = document.querySelector('#toggler');
+const nav = document.querySelector('.header__wrapper');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('header__wrapper--active');
+  toggler.classList.toggle('header__burger--active');
+}
+);
