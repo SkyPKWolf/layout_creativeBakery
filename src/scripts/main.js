051ecc0afb54e689a3d81129c90e84281179caa9
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-04-19 19:27:18 +0300

    v0.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,17 @@
 
 const toggler = document.querySelector('#toggler');
 const nav = document.querySelector('.header__wrapper');
+const navLink = document.querySelectorAll('.nav__link');
 
 toggler.addEventListener('click', () => {
   nav.classList.toggle('header__wrapper--active');
   toggler.classList.toggle('header__burger--active');
 }
 );
+
+for (const link of navLink) {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('header__wrapper--active');
+    toggler.classList.toggle('header__burger--active');
+  });
+}
